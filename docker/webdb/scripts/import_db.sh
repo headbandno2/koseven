68fe97891669c701d9479a17b1fdb4f94bb16183
Andrius Každailis <andrius.kazdailevicius@gmail.com>
2018-05-02 15:46:08 +0200

    Updated import script

--- docker/webdb/scripts/import_db.sh
+++ docker/webdb/scripts/import_db.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CONNECT_HOSTNAME="127.0.0.1"
-DATABASE_NAME='koseven_localhost'
-mysql --defaults-file="$DIR/my.cnf" --host=$CONNECT_HOSTNAME -e "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql \
+	--defaults-file="/scripts/my.cnf" \
+	-e "CREATE DATABASE IF NOT EXISTS koseven_localhost CHARACTER SET utf8 COLLATE utf8_general_ci"
